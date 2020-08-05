05daec641253a3b72a52aa1e7761b56d99aa8864
Poololiu <liuyuanbolyb@gmail.com>
2020-08-06 07:44:44 +0800

    1.修改了激光武器弹射逻辑,激光的弹射现在由子弹实现

--- Assets/Scripts/Weapon/Raycast.cs
+++ Assets/Scripts/Weapon/Raycast.cs
@@ -7,6 +7,8 @@ public class Raycast : MonoBehaviour
     public SpriteRenderer sr;
     public float damage;
     public Weapon fireWeapon;
+    public int reboundResidueTimes;
+    private string[] maskLayer;
     // Start is called before the first frame update
 
     private void Awake()
@@ -17,11 +19,13 @@ public class Raycast : MonoBehaviour
     {
         
     }
-    public void ConfigRaycast(float direction,float _damage,Weapon _fireWeapon)
+    public void ConfigRaycast(float direction,float _damage,Weapon _fireWeapon,int _reboundResidueTimes, string[] _maskLayer)
     {
         sr.size = new Vector2(direction, 1);
         damage = _damage;
         fireWeapon = _fireWeapon;
+        reboundResidueTimes = _reboundResidueTimes;
+        maskLayer = _maskLayer;
     }
 
     // Update is called once per frame
@@ -48,6 +52,38 @@ public class Raycast : MonoBehaviour
             NPC npc = other.GetComponentInChildren<NPC>();
             npc.ReceiveDamageWithRepelVector(damage,transform.right);
         }
+    }
+
+    public void ReboundRaycast(RaycastHit2D preHit, float residueDistance, Quaternion preRotation, Vector2 incomingVector)
+    {
+            //获得反射向量
+            Vector2 reflectVector = Vector2.Reflect(incomingVector, preHit.normal);
+
+            RaycastHit2D[] hits = Physics2D.RaycastAll(preHit.point, reflectVector, residueDistance, LayerMask.GetMask(maskLayer));
+            if (hits.Length > 0) { 
+                RaycastHit2D hit = hits[0];
+                if (hits[0].fraction == 0)
+                {
+                    if (hits.Length >= 2)
+                    {
+                        hit = hits[1];
+                    }
+                }
+
+                if (hit.collider)
+                {
+                    Raycast raycast = Instantiate((GameObject)Resources.Load("Bullet/Raycast/Raycast")).GetComponent<Raycast>();
+                    raycast.transform.position = preHit.point;
+                    raycast.transform.rotation = preRotation * Quaternion.FromToRotation(incomingVector, reflectVector);
+                    float currDistance = hit.fraction * residueDistance;
+                    raycast.ConfigRaycast(currDistance, damage, fireWeapon, reboundResidueTimes-1,maskLayer);
+                    if (reboundResidueTimes > 0)
+                    {
+                        raycast.ReboundRaycast(hit, residueDistance * (1 - hit.fraction), raycast.transform.rotation, reflectVector);
+                    }
+                }
+            }
+
     }
 
 
